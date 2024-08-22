ba46ca8d052bce37d774753744242d3951b60a3c
orangefoxl <romanolar@mail.ru>
2024-08-22 18:51:11 +0400

    Фмкс точности спарклайна #10

--- features/basic.py
+++ features/basic.py
@@ -40,7 +40,7 @@ def draw_sparkline(values=[0, 1, 2, 3, 4, 5, 6, 7, 8, -8, -7, -6, -5, -4, -3, -2
 
     sparkline = ''
     for value in values:
-        index = round(abs(value) * scale)
+        index = int(abs(value) * scale)
 
         if index >= len(symbols):
             index = len(symbols) - 1
